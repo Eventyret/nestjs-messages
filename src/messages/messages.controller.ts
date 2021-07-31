0cf507d96d91588bb11bba0a4a7874052cd1ce4c
Simen Daehlin <simen.dehlin@gmail.com>
2021-07-31 08:39:57 +0100

    Added bad way of dependancy injection

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,7 +1,12 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 @Controller('messages')
 export class MessagesController {
+  messageService: MessagesService;
+  constructor() {
+    this.messageService = new MessagesService();
+  }
   @Get('/')
   listMessages() {
     return 'Hello There';
