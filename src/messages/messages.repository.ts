d267ae6a0a89bc12188817dbcd09c3c6685cfecc
Simen Daehlin <simen.dehlin@gmail.com>
2021-07-31 08:31:55 +0100

    Created Create method

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -5,6 +5,18 @@ export class MessagesRepository {
     const messages = JSON.parse(contents);
     return messages[id];
   }
-  async findAll() {}
-  async create(message: string) {}
+  async findAll() {
+    const content = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(content);
+    return messages;
+  }
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id: content };
+
+    await writeFile('messages.json', JSON.stringify(messages), 'utf8');
+  }
 }
